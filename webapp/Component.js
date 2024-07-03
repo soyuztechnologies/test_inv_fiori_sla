3782cbb17ccd02c118bdb81812895d13581fca1e
Anubhav Oberoi <monday.ui5@gmail.com>
2024-07-03 11:35:56 +0000

    stable change

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("inv.ao.managecust.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
